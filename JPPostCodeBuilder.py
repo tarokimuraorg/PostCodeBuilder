0cbe816957c814bc0bfbfc3a64f6d26f07860210
Taro Kimura <tarokimuraorg@outlook.com>
2022-06-13 14:13:16 +0900

    first commit

---
+++ JPPostCodeBuilder.py
@@ -0,0 +1,48 @@
+import re
+from ErrorMessageCreator import ErrorMessageCreator
+
+class JPPostCodeBuilder:
+
+    def __init__(self, post_code : str):
+
+        self._post_code = ''
+        self._emcreator = ErrorMessageCreator()
+        
+        if re.match('^\d{3}-\d{4}$', post_code):
+            self._post_code = post_code.replace('-','')
+        
+        if re.match('^\d{7}$', post_code):
+            self._post_code = post_code
+
+    def digits_only(self) -> str:
+
+        if (re.match('^\d{7}$',self._post_code)):
+            return self._post_code
+
+        raise ValueError(self._emcreator.message('JPPostCodeBuilder.py','init','invalid argument','the argument is an incompatible japan post code.'))
+
+    def convertToAddress(self):
+
+        if self._post_code == '0791143':
+            return '北海', '道', '赤平', '市'
+
+    def convertKanjiToYomigana(self):
+
+        address = self.convertToAddress()
+        yomigana = []
+
+        for kanji in address:
+
+            if kanji == '道':
+                yomigana.append('ドウ')
+            
+            if kanji == '市':
+                yomigana.append('シ')                
+
+            if kanji == '北海':
+                yomigana.append('ホッカイ')
+                
+            if kanji == '赤平':
+                yomigana.append('アカビラ')
+
+        return yomigana
