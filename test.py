82fd869f1dae067eb2abd6ebc5d720215f56d460
Taro Kimura <tarokimuraorg@outlook.com>
2022-06-15 14:36:58 +0900

    テストする郵便番号を追加

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from JPPostCodeBuilder import JPPostCodeBuilder
 
-post_code = '079-1143'
-jppc_builder = JPPostCodeBuilder(post_code)
-address = jppc_builder.convertToAddress()
+post_code = ''
+#post_code = '079-1143'
+#post_code = '079-1134'
+#post_code = '079-1274'
+#post_code = '079-1141'
+#post_code = '079-1121'
 
-print(jppc_builder.convertToAddress())
-print(jppc_builder.convertKanjiToYomigana())
+try:
+
+    jppc_builder = JPPostCodeBuilder(post_code)
+    address = jppc_builder.convertToAddress()
+
+    print(jppc_builder.convertToAddress())
+    print(jppc_builder.convertKanjiToYomigana())
+
+except ValueError as ve:
+    print(ve)
