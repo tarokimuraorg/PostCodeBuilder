5c8ee04ad37038277125ab17ac785f909eb928ce
Taro Kimura <tarokimuraorg@outlook.com>
2022-06-15 14:34:24 +0900

    一行目に改行コードを追加

--- ErrorMessageCreator.py
+++ ErrorMessageCreator.py
@@ -2,7 +2,7 @@ class ErrorMessageCreator:
 
     def message(self, file_name : str, function_name : str, error_type : str, error_message : str) -> str:
 
-        message = ''
+        message = '\n'
         message += '   error type : {}\n'.format(error_type)
         message += 'error message : {}\n'.format(error_message)
         message += '    file name : {}\n'.format(file_name)
