5dc949a91f267cd4e80f6bfcdde7a590df1fe1c6
Taro Kimura <tarokimuraorg@outlook.com>
2022-06-16 14:30:50 +0900

    テストする郵便番号を追加

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from turtle import pos
 from JPPostCodeBuilder import JPPostCodeBuilder
 
 post_code = ''
@@ -6,9 +7,18 @@ post_code = ''
 #post_code = '079-1274'
 #post_code = '079-1141'
 #post_code = '079-1121'
+#post_code = '079-1101'
+#post_code = '079-1154'
+#post_code = '079-1152'
+#post_code = '079-1155'
+#post_code = '079-1156'
+#post_code = '079-1124'
+#post_code = '079-1133'
+#post_code = '079-1135'
+#post_code = '079-1131'
+post_code = '079-1122'
 
 try:
-
     jppc_builder = JPPostCodeBuilder(post_code)
     address = jppc_builder.convertToAddress()
 
