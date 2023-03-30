0c4fa3bc7750c0cd1a25cd23e463ef838ff1b800
Taro Kimura <tarokimuraorg@outlook.com>
2023-03-30 16:11:50 +0900

    仕様変更
    ・郵便番号の下4桁が「0000」以外にも例外的な処理が必要な番号があることが判明

--- JPPostCodeBuilder.py
+++ JPPostCodeBuilder.py
@@ -1,6 +1,6 @@
 import re
-import dataclasses
 from JPAddressBook import JPAddressBook
+from JPAddressPage import JPAddressPage
 from StringConvertor import StringConvertor
 import ErrorMessageBuilder
 
@@ -16,30 +16,22 @@ class JPPostCodeBuilder:
         
         if re.match('^\d{7}$', out_code):
             self._post_code = out_code
-        else:
-            print(ErrorMessageBuilder.message('JPPostCodeBuilder','address_finder','invalid argument','the post code is a 7-digit number.'))
 
     def address_finder(self):
 
         if self._post_code:
 
             address_book = JPAddressBook().createAddressBook()
-            address_pages = list(filter(lambda page: (page.post_code == self._post_code) or \
-                               (page.post_code[3:] == '0000' and \
-                                page.post_code[0:3] == self._post_code[0:3]), address_book))
+            address_pages = list(filter(lambda page: page.post_code == self._post_code, address_book))
 
-            if len(address_pages) > 1:
-                return list(filter(lambda page: page.post_code[3:] != '0000', address_pages))
+            if (self._post_code[0:3] == '079'):
 
-            if len(address_pages) == 1:
-
-                page = address_pages[0]
-
-                if page.post_code[3:] == '0000':
-                    return [dataclasses.replace(page, post_code=self._post_code)]
-                else:
+                if len(address_pages) > 0:
                     return address_pages
-    
-            print(ErrorMessageBuilder.message('JPPostCodeBuilder','address_finder','invalid argument',f'the post code {self._post_code} is an incompatible value.'))
+                
+                return JPAddressPage(self._post_code, '北海道 赤平市', 'ﾎｯｶｲﾄﾞｳ ｱｶﾋﾞﾗｼ')
 
+            return address_pages
+    
+        print(ErrorMessageBuilder.message('JPPostCodeBuilder','address_finder','invalid argument','the post code is a 7-digit number.'))
         return []
