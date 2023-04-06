88418215661658bca68b13bf937fb854ddf23d05
Taro Kimura <tarokimuraorg@outlook.com>
2023-04-06 16:24:17 +0900

    仕様変更に対応
    ・処理速度を上げるため、すべてのデータをJPAddressPageに変換する処理を削除

---
+++ JPAddressData.py
@@ -0,0 +1,18 @@
+import pandas
+import ErrorMessageBuilder
+
+def read_address_data():
+
+    address_book = []
+
+    try:
+
+        data_frame = pandas.read_csv('./csv/ken_all.csv', usecols=[2,3,4,5,6,7,8], names=['post_code','furigana1','furigana2','furigana3','address1','address2','address3'])
+        csv_data = data_frame.drop_duplicates()
+
+        return csv_data.values.tolist()
+    
+    except Exception as e:
+        print(ErrorMessageBuilder.message('JPAddressData', 'readAddressData', 'failed to read csv data', f'{e}'))
+    
+    return address_book
